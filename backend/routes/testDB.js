8e5f9f8849f0dd3e684475e38bbb482a4cc72fff
Ichiky_Otsu <66952647+Brice66@users.noreply.github.com>
2024-03-11 01:40:39 +0100

    Ajout de la configuration de la base de données PostgreSQL

---
+++ backend/routes/testDB.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router(); // Utilisez Router au lieu de créer une nouvelle application Express
+const pool = require('../db');
+
+// Modifier ici pour utiliser `router.get` au lieu de `app.get`
+router.get('/', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM todos');
+    res.json(rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router; // Exportez le router au lieu d'écouter sur un port
