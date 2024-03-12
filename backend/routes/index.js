51adf2bc0652dae7b29e448b324273b06555b877
Kirato <justkirato@gmail.com>
2024-03-12 17:22:12 +0100

    change backend hello text

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/api/hello', function(req, res, next) {
-  res.json({ message: 'Hello from Express Backend' });
+router.get('/api/hello', function (req, res, next) {
+    res.json({message: 'Hello PL from Express Backend'});
 });
 
 module.exports = router;
