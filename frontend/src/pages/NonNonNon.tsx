2a1a00810bea9404a366e8e03fb0e48c25cdcb6c
Ichiky_Otsu <66952647+Brice66@users.noreply.github.com>
2024-04-11 11:43:39 +0200

    Problème de css dispertion de NonNonNon fixed

--- frontend/src/pages/NonNonNon.tsx
+++ frontend/src/pages/NonNonNon.tsx
@@ -1,18 +1,19 @@
 // NonNonNon.tsx
 import { Component } from 'solid-js';
 import { A } from '@solidjs/router';
-import './NonNonNon.css';
+import styles from './NonNonNon.module.css';
 
 const NonNonNon = () => {
     return (
-        <div class="container">
-            <div class="meme">
+        <div class={styles.nonNonNonContainer}>
+            <div class={styles.nonNonNonMeme}>
                 <img src="https://i.imgflip.com/4/4t0m5.jpg" alt="Nope meme" />
                 <h1>Non, non, non !</h1>
                 <p>Vous n'êtes pas autorisé à accéder à cette page.</p>
-                <A href="/" class="button">Retourner à l'accueil</A>
+                <A href="/" class={styles.nonNonNonButton}>Retourner à l'accueil</A>
             </div>
         </div>
         );
 };
+
 export default NonNonNon;
