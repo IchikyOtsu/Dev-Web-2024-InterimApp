a6dc15e40cf0dbb55eda969be8f55e902ed0ef3c
Alocrl <aloischarlier100@gmail.com>
2024-04-12 16:51:52 +0200

    css et required pour profil + icons navbar

--- frontend/src/Components/Navbar/index.tsx
+++ frontend/src/Components/Navbar/index.tsx
@@ -1,34 +1,27 @@
 import { A } from "@solidjs/router";
 // Navbar.tsx
 import "./index.css";
-
+import { ImDrawer } from 'solid-icons/im'
+import { FaSolidCalendarDays } from 'solid-icons/fa'
+import { AiFillWechat } from 'solid-icons/ai'
+import { FaSolidChartSimple } from 'solid-icons/fa'
+import { CgProfile } from 'solid-icons/cg'
 export const Navbar = () => {
 	return (
 		<>
-			<A href="/" class="logo">
-				Proxideal
-			</A>
+			<A href="/" class="logo">Proxideal</A>
 
 			<nav class="navbar">
-				<A href="/adverts" class="active">
-					Adverts<i class="bx bxs-inbox"></i>
-				</A>
-				<A href="/planning">
-					Planning<i class="bx bxs-calendar"></i>
-				</A>
-				<A href="/chat">
-					Chat<i class="bx bxs-chat"></i>
-				</A>
-				<A href="/notifications">
-					Notifications<i class="bx bxs-bar-chart-alt-2"></i>
-				</A>
-				<A href="/profile">
-					Profile<i class="bx bxs-user"></i>
-				</A>
+				<A href="/adverts" class="active">Adverts<ImDrawer class="nav-icon"/></A>
+				<A href="/planning">Planning<FaSolidCalendarDays class="nav-icon"/></A>
+				<A href="/chat">Chat<AiFillWechat class="nav-icon"/></A>
+				<A href="/tracking">Tracking<FaSolidChartSimple class="nav-icon"/></A>
+				<A href="/profile">Profile<CgProfile class="nav-icon"/></A>
 			</nav>
 		</>
-	);
+		);
 };
+
 
 export const EnterpriseNavbar = () => {
 	return (
---
+++ frontend/src/pages/Adverts.tsx
@@ -0,0 +1,29 @@
+// Adverts.tsx
+import { createEffect, createSignal } from 'solid-js';
+import CardAdvert from '../Components/Advert/index.tsx'; // Vérifiez le chemin d'accès
+import styles from './Adverts.module.css';
+
+const AdvertsPage = () => {
+  const [adverts, setAdverts] = createSignal([]);
+
+  createEffect(() => {
+    fetch('/api/adverts')
+      .then(res => res.json())
+      .then(data => setAdverts(data)) 
+      .catch(err => console.error("API call failed:", err));
+  });
+
+  return (
+    <div>
+      <h1>Page Adverts</h1>
+      <ul class={styles.advertsContainer}>
+        {adverts().map(ad => (
+          // Assurez-vous que les champs passés correspondent aux données attendues par CardAdvert
+          <CardAdvert key={ad.id} title={ad.title} message={ad.message} location={ad.location} time={ad.time} duration={ad.duration} date={ad.date} />
+          ))}
+      </ul>
+    </div>
+    );
+}
+
+export default AdvertsPage;
