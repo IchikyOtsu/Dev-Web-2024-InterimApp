76d8e30ffb2c601aa5a2abbef38cf56ffaaf0274
Kirato77 <justkirato@gmail.com>
2024-04-25 23:54:52 +0200

    profile: update styles

--- frontend/src/Components/Profile/index.tsx
+++ frontend/src/Components/Profile/index.tsx
@@ -1,4 +1,12 @@
-import { Button, Input, Select, Text } from "@jundao/design";
+import {
+	Button,
+	Card,
+	Image,
+	Input,
+	Select,
+	Space,
+	Text,
+} from "@jundao/design";
 
 import { createSignal, onMount } from "solid-js";
 import { useGlobalContext } from "../../context";
@@ -67,128 +75,110 @@ const Profile = ({ profilePicture }) => {
 	};
 
 	return (
-		<div class="profile-card">
-			<div class="left-section">
-				<img src={profilePicture} alt="Profile" class="profile-picture" />
-				<Text class="text" id="text-avatar">
-					NomPrénom
-				</Text>
-				<br />
-				<Text class="text" id="text-avatar">
-					nomprenom@email.com
-				</Text>
-			</div>
-			<div class="right-section">
-				<div class="input-group">
-					<div id="nom">
-						<Text class="text"> Nom </Text>
+		<Card class="profile-card" style="margin: 1rem">
+			<Space align="center" wrap style="justify-content: center;">
+				<Space vertical class="left-section" align="center">
+					<Image
+						src={profilePicture}
+						alt="Profile"
+						shape="circle"
+						class="profile-picture"
+					/>
+					<Text>{`${prenom()} ${nom()}`}</Text>
+					<Text>{userData().email}</Text>
+				</Space>
+				<Space vertical class="right-section">
+					<Space wrap>
+						<Space vertical>
+							<Text bold> Nom </Text>
+							<Input
+								type="text"
+								placeholder="Nom de famille"
+								value={nom()}
+								onChange={(nom) => setNom(nom)}
+								required
+							/>
+						</Space>
+						<Space vertical>
+							<Text bold> Prénom </Text>
+							<Input
+								type="text"
+								placeholder="Prénom"
+								value={prenom()}
+								onChange={(prenom) => setPrenom(prenom)}
+								required
+							/>
+						</Space>
+					</Space>
+					<Space vertical>
+						<Text bold> Adresse mail </Text>
 						<Input
-							id="nom-input"
-							class="input-nom"
+							type="email"
+							placeholder="nomprenom@gmail.com"
+							value={userData().email}
+							required
+							disabled
+						/>
+					</Space>
+					<Space vertical>
+						<Text bold> Compétences </Text>
+						<Input
+							id="competences-input"
+							class="input-competence"
 							type="text"
-							placeholder="Nom de famille"
-							value={nom()}
-							onChange={(nom) => setNom(nom)}
+							placeholder="Ex: Informatique"
 							required
 						/>
-					</div>
-					<div id="prenom">
-						<Text class="text"> Prénom </Text>
+					</Space>
+					<Space wrap>
+						<Space vertical>
+							<Text bold> Code Postal </Text>
+							<Input
+								type="number"
+								placeholder="9999"
+								value={codePostal()}
+								onChange={(codePostal) => setCodePostal(codePostal)}
+								required
+							/>
+						</Space>
+						<Space vertical>
+							<Text bold> Ville </Text>
+							<Select
+								placeholder={"Sélectionnez une ville"}
+								options={["Lyon", "Bruxelles", "Leuven"]}
+								value={ville()}
+								onChange={(ville: string) => setVille(ville)}
+								required
+							/>
+						</Space>
+					</Space>
+					<Space vertical>
+						<Text bold> Adresse </Text>
 						<Input
-							id="prenom-input"
-							class="input-prenom"
 							type="text"
-							placeholder="Prénom"
-							value={prenom()}
-							onChange={(prenom) => setPrenom(prenom)}
+							placeholder="Rue de la place, 14"
+							value={addresse()}
+							onChange={(addresse) => setAddresse(addresse)}
 							required
 						/>
-					</div>
-				</div>
-				<div id="mail">
-					<Text class="text"> Adresse mail </Text>
-					<Input
-						id="mail-input"
-						class="input-mail"
-						type="email"
-						placeholder="nomprenom@gmail.com"
-						value={userData().email}
-						required
-						disabled
-					/>
-				</div>
-				<div id="competences">
-					<Text class="text"> Compétences </Text>
-					<Input
-						id="competences-input"
-						class="input-competence"
-						type="text"
-						placeholder="Ex: Informatique"
-						value={competence()}
-						onChange={(competence) => setCompetence(competence)}
-						required
-					/>
-				</div>
-				<div class="input-group">
-					<div id="code-postal">
-						<Text class="text"> Code Postal </Text>
+					</Space>
+					<Space vertical>
+						<Text bold> Documents </Text>
 						<Input
-							id="code-postal-input"
-							class="input-code-postal"
-							type="number"
-							placeholder="9999"
-							value={codePostal()}
-							onChange={(codePostal) => setCodePostal(codePostal)}
+							type="file"
+							accept="image/png, image/jpeg"
+							multiple
 							required
 						/>
-					</div>
-					<div id="ville">
-						<Text class="text"> Ville </Text>
-						<Select
-							id="ville-input"
-							class="input-ville"
-							placeholder={"Sélectionnez une ville"}
-							options={["Lyon", "Bruxelles", "Leuven"]}
-							value={ville()}
-							onChange={(ville: string) => setVille(ville)}
-							required
-						/>
-					</div>
-				</div>
-				<div id="adresse">
-					<Text class="text"> Adresse </Text>
-					<Input
-						id="adresse-input"
-						class="input-adresse"
-						type="text"
-						placeholder="Rue de la place, 14"
-						value={addresse()}
-						onChange={(addresse) => setAddresse(addresse)}
-						required
+					</Space>
+					<Button
+						children="Enregistrer"
+						type="primary"
+						onClick={handleSubmit}
 					/>
-				</div>
-				<div id="documents">
-					<Text class="text"> Documents </Text>
-					<br></br>
-					<input
-						id="documents-input"
-						class="input-documents"
-						type="file"
-						accept="image/png, image/jpeg"
-						value={document()}
-						onChange={(document) => setDocument(document)}
-						multiple
-						required
-					/>
-				</div>
-				<Button
-					class="send-button"
-					children="Envoyer"
-					type="primary"
-					onClick={handleSubmit}
-				/>
-			</div>
-		</div>
+				</Space>
+			</Space>
+		</Card>
 	);
 };
 
